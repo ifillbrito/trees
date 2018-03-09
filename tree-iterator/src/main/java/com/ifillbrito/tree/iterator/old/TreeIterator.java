42d45b5ab90809bbb75daa6cc9b0c37bb0f5cfa7
ifillbrito <grebiel.ifillbrito@gmail.com>
2018-03-09 22:49:56 +0100

    old implementation deleted.

--- tree-iterator/src/main/java/com/ifillbrito/tree/iterator/old/TreeIterator.java
+++
@@ -1,9 +0,0 @@
-package com.ifillbrito.tree.iterator.old;
-
-/**
- * Created by gjib on 07.01.18.
- */
-public interface TreeIterator<Root>
-{
-    TreeIterator<Root> execute();
-}
--- tree-iterator/src/test/java/com/ifillbrito/tree/example/impl/singletype/input/render.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-dot tree.dot -Tps -o tree.ps;
-convert -flatten -density 150 -geometry 100% tree.ps tree.png;
